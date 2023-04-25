c315b6af8c96c268dadfa3e026996b64bc26130a
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-25 16:34:38 +0300

    add

--- test/_printf.c
+++ test/_printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /** 
-  * _printf - printf function 
-  * @format: arguments 
-  * Return: number of characters printed 
-  */
+* _printf - printf function 
+* @format: arguments 
+* Return: number of characters printed 
+*/
 
 int _printf(const char *format, ...)
 {
@@ -11,27 +11,27 @@ va_list arguments;
 const char *p;
 int num = 0;
 va_start(arguments, format);
-if (format == NULL || (format[0] == % && format[1] == 0))
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 for (p = format; *p; p++)
 {
-if (*p == % &&  *p + 1 == %)
+if (*p == '%' &&  *p + 1 == '%')
 {
 _putchar(*p), num++;
 continue;
 }
-else if (*p == %  &&  *p + 1 != %)
+else if (*p == '%'  &&  *p + 1 != '%')
 {
 switch (*++p)
 {
-case s: num += fun_string(arguments);
+case 's': num += fun_string(arguments);
 break;
-case c: num += fun_character(arguments);
+case 'c': num += fun_character(arguments);
 break;
-case % : _putchar( % ), num++;
+case '%': _putchar '%'), num++;
 break;
-case i: case d: num += fun_integer(arguments); break;
-default: _putchar( % ), _putchar(*p), num += 2;
+case 'i': case d: num += fun_integer(arguments); break;
+default: _putchar( '%' ), _putchar(*p), num += 2;
 }
 }
 else
